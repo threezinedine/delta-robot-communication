5ff726b0fadfd25f5b8dbb3e166d685582204369
unknown <threezinedine@gmail.com>
2022-09-10 15:24:59 +0700

    refactor to_hex function with 6

--- test/command_test.py
+++ test/command_test.py
@@ -4,9 +4,11 @@ from src.models import Command
 
 class CommandTest(unittest.TestCase):
     def test_command_with_command_address_to_hex_function(self):
+        function_address = 6
+        expected_func = b'\x30\x30\x00\x00\x00\x22\x01\x06\x00\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
         command = Command()
-        command.set_function(6)
+        command.set_function(function_address)
 
         result = command.to_hex()
 
-        assert result == b'\x30\x30\x00\x00\x00\x22\x01\x06\x00\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        assert result == expected_func
