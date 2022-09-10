61bbf6aae470e05cc2b8ba014d9df4e58e2d06e9
unknown <threezinedine@gmail.com>
2022-09-10 17:12:43 +0700

    refactor property

--- src/models/properties/property.py
+++ src/models/properties/property.py
@@ -2,11 +2,14 @@ from .i_property import ISetValuableProperty
 
 
 class Property(ISetValuableProperty):
+    BYTEORDER = 'big'
+
     def __init__(self):
         self._value = 0
+        self._num_bytes = 2
     
     def to_hex(self) -> bytearray:
-        return self._value.to_bytes(2, byteorder='big')
+        return self._value.to_bytes(self._num_bytes, byteorder=self.BYTEORDER)
 
     def set_value(self, new_value:object) -> None:
         self._value = new_value
