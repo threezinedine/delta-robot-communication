2c5dafe6a7ba39c921a8969a538d3db412e76c81
unknown <threezinedine@gmail.com>
2022-09-10 17:07:05 +0700

    refactor the property test

--- test/property_test.py
+++ test/property_test.py
@@ -4,9 +4,12 @@ from src.models.properties import Property
 
 class PropertyTest(unittest.TestCase):
     def test_to_hex_with_value_is_6(self):
+        new_value = 6
+        expected_result = b'\x00\x06'
+
         address_property = Property() 
-        address_property.set_value(6)
+        address_property.set_value(new_value)
 
         result = address_property.to_hex()
 
-        assert result == b'\x00\x06'
+        assert result == expected_result
