eb5759794a5f4afeb14567956c0bebe5c7e8cc55
unknown <threezinedine@gmail.com>
2022-09-10 15:27:03 +0700

    add new test for to_hex function with function address 7

--- test/command_test.py
+++ test/command_test.py
@@ -12,3 +12,15 @@ class CommandTest(unittest.TestCase):
         result = command.to_hex()
 
         assert result == expected_func
+
+    def test_command_with_command_address_4_to_hex_function(self):
+        function_address = 7
+        expected_func = b'\x30\x30\x00\x00\x00\x22\x01\x06\x00\x07\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        command = Command()
+        command.set_function(function_address)
+
+        result = command.to_hex()
+
+        assert result == expected_func
+
+
