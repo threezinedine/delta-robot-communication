c2f2d8fdea33113ed7860aa417b8eb07382769c1
unknown <threezinedine@gmail.com>
2022-09-10 16:15:37 +0700

    add test to_hex function with address 4 and change the 5th param to 3

--- test/command_test.py
+++ test/command_test.py
@@ -35,3 +35,19 @@ class CommandTest(unittest.TestCase):
         command.set_function(function_address)
 
         address_property.set_value.assert_called_once_with(function_address)
+
+
+    def test_command_with_command_address_4_to_hex_function_and_change_the_param_5th(self):
+        function_address = 4
+        param_index = 5
+        param_value = 3
+        expected_func = b'\x30\x30\x00\x00\x00\x22\x01\x06\x00\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        address_property = Mock(spec=ISetValuableProperty)
+        address_property.to_hex.return_value = b'\x00\x04'
+        command = Command(address_property=address_property)
+        command.set_function(function_address)
+        command.set_param_value(param_index, param_value)
+
+        result = command.to_hex()
+
+        assert result == expected_func
