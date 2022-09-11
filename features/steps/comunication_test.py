69678f8af83617c7037eed8648a3280eeecb84bb
unknown <threezinedine@gmail.com>
2022-09-11 14:01:48 +0700

    pass the acceptance test for change property

--- features/steps/comunication_test.py
+++ features/steps/comunication_test.py
@@ -17,8 +17,8 @@ def when_the_command_is_set_to_address(context, function):
 
 @when("the command's {param_index}th param is changable, and is modified to {param_value}")
 def the_param_of_the_command_is_changed(context, param_index, param_value):
-    context.command.set_param(param_index, Property(num_bytes=4))
-    context.command.set_param_value(param_index, param_value)
+    context.command.set_param(int(param_index), Property(num_bytes=4))
+    context.command.set_param_value(int(param_index), int(param_value))
 
 @when("the app sends that command")
 def when_set_the_function_for_the_command_and_send(context):
