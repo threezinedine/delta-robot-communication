43085cd0400bc5c2fac0c7d01019ad826c983f8d
unknown <threezinedine@gmail.com>
2022-09-11 13:20:53 +0700

    refactor the test and add an new way to get the default command (address 6)

--- features/communication.feature
+++ features/communication.feature
@@ -1,5 +1,10 @@
 Feature: Communicate with the server by specific tasks
     Scenario: Send a stop command
         Given the server is binded as 127.0.0.1:1234
-        When the app sends the stop command (address 6)
+        When the app sends the default command
+        Then the app should receive the response
+
+    Scenario: Send a command with the address function 6
+        Given the server is binded as 127.0.0.1:1234 
+        When thap app sends the command with address 6
         Then the app should receive the response
--- features/steps/comunication_test.py
+++ features/steps/comunication_test.py
@@ -1,21 +1,24 @@
 from behave import given, when, then
 from src.controllers import Controller
 from src.models import Command
+from src.models.properties import Property
 
-
-
-@when("the app sends the stop command (address {function})")
-def when_the_stop_function_is_sent(context, function):
+@when("the app sends the default command")
+def when_the_stop_function_is_sent(context):
     context.command = Command()
     context.controller = Controller(command=context.command)
     context.controller.connect(context.ip_address, context.port)
-    print(context.command.to_hex())
+    context.response = context.controller.send(context.controller.command.to_hex())
+
+@when("thap app sends the command with address {function}")
+def when_set_the_function_for_the_command_and_send(context, function):
+    address_property = Property()
+    context.command = Command(address_property=address_property) 
+    context.command.set_function(int(function))
+    context.controller = Controller(command=context.command)
+    context.controller.connect(context.ip_address, context.port)
     context.response = context.controller.send(context.controller.command.to_hex())
 
 @then("the app should receive the response")
 def then_the_app_receive_the_response_of_the_stop_command(context):
-    assert context.response == b'\x30\x30\x00\x00\x00\x08\x01\x03\x00\x06\x00\x00\x00\x00'
-
-@then("the command should have the param {index} is None")
-def and_check_the_command_param(context):
-    assert context.controller.command.get_param(int(index)) == None
+    assert context.response != b'fail'
