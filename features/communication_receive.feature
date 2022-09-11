47bd068aef397f00aeeceb3a725a567525c14fa4
unknown <threezinedine@gmail.com>
2022-09-11 14:52:53 +0700

    add acceptance test for response

--- features/communication_receive.feature
+++ features/communication_receive.feature
@@ -1,6 +1,6 @@
 Feature: Receive response from the server
     Scenario: Receive a command from the server 
         Given the server is binded as 127.0.0.1:1234
-        When the command is set to default
+        When the command is set to address 5
         And the app sends that command
-        Then the app should receive the address "6" and value "0"
+        Then the app should receive the address "5" and value "0"
--- features/steps/comunication_receive_test.py
+++ features/steps/comunication_receive_test.py
@@ -3,7 +3,9 @@ from behave import given, when, then
 
 @then("the app should receive the address \"{address}\" and value \"{value}\"")
 def then_the_app_receive_the_address_and_value(context, address, value):
-    response = Response.from_bytes(context.response)
+    print(context.response)
+    response = Response()
+    Response.from_bytes(response, context.response)
 
     assert response.get_function() == address
     assert response.get_value() == value
