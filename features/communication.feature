f41234bd774388c452862b1c6cfef3cfc96cc86c
unknown <threezinedine@gmail.com>
2022-09-11 13:37:48 +0700

    add send command with changable param acceptance test

--- features/communication.feature
+++ features/communication.feature
@@ -1,10 +1,19 @@
 Feature: Communicate with the server by specific tasks
     Scenario: Send a stop command
         Given the server is binded as 127.0.0.1:1234
-        When the app sends the default command
+        When the command is set to default
+        And the app sends that command
         Then the app should receive the response
 
     Scenario: Send a command with the address function 6
         Given the server is binded as 127.0.0.1:1234 
-        When thap app sends the command with address 6
+        When the command is set to address 6
+        And the app sends that command
+        Then the app should receive the response
+
+    Scenario: Send a command with the address function 4, the 4th param is modified to 1523
+        Given the server is binded as 127.0.0.1:1234 
+        When the command is set to address 4
+        And the command's 4th param is changable, and is modified to 1523
+        And the app sends that command
         Then the app should receive the response
--- features/steps/comunication_test.py
+++ features/steps/comunication_test.py
@@ -3,19 +3,25 @@ from src.controllers import Controller
 from src.models import Command
 from src.models.properties import Property
 
-@when("the app sends the default command")
-def when_the_stop_function_is_sent(context):
+@when("the command is set to default")
+def when_the_default_is_set(context):
     context.command = Command()
     context.controller = Controller(command=context.command)
-    context.controller.connect(context.ip_address, context.port)
-    context.response = context.controller.send(context.controller.command.to_hex())
 
-@when("thap app sends the command with address {function}")
-def when_set_the_function_for_the_command_and_send(context, function):
+@when("the command is set to address {function}")
+def when_the_command_is_set_to_address(context, function):
     address_property = Property()
     context.command = Command(address_property=address_property) 
     context.command.set_function(int(function))
     context.controller = Controller(command=context.command)
+
+@when("the command's {param_index}th param is changable, and is modified to {param_value}")
+def the_param_of_the_command_is_changed(context, param_index, param_value):
+    context.command.set_param(param_index, Property(num_bytes=4))
+    context.command.set_param_value(param_index, param_value)
+
+@when("the app sends that command")
+def when_set_the_function_for_the_command_and_send(context):
     context.controller.connect(context.ip_address, context.port)
     context.response = context.controller.send(context.controller.command.to_hex())
 
