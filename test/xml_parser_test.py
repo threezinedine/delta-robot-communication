51b2f9416448f648140ee473ef05fa1d977b6546
unknown <threezinedine@gmail.com>
2022-09-13 14:47:12 +0700

    update requirement.txt with dict2xml package

--- test/xml_parser_test.py
+++ test/xml_parser_test.py
@@ -6,11 +6,16 @@ from src.controllers import XMLParser
 class XMLParserTest(unittest.TestCase):
     filename="test.xml"
     header = "<?xml version=\"1.0\"?>"
+
     def create_the_xml_file(self, commands=[]):
         with open(self.filename, 'w') as file:
             file.write("<commands>\n")
             for command in commands:
-                pass
+                for key, value in command.items():
+                    if not isinstance(value, dict):
+                        file.write(f"\t<{key}>{value}</{key}>\n")
+                    else:
+                        for key_2, value_2 in value.items():
             file.write("</commands>\n")
 
     def test_parse_no_commands_xml_file(self):
@@ -20,3 +25,6 @@ class XMLParserTest(unittest.TestCase):
         assert parser.get_commands_list() == []
 
         
+    def test_parse_stop_command_from_xml_file(self):
+        self.create_the_xml_file(commands=[{"name": "stop", "address": 6}])
+
